936b60d26de655c112df003faa07dc642aa331d9
Jo Emil <jo.emil@skrivelief.com>
2014-03-16 15:08:21 +0100

    Implemented cheats

--- server/server.js
+++ server/server.js
@@ -5,11 +5,8 @@ var PORT = 54321;
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
-
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
-    client.write('I am Chuck Norris!');
-
+    client.write('martinerkul');
 });
 
 client.on('error', function(data) {
