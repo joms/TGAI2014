65168fbd865f53d1b90cd0f3ff428f82437cb030
Jo Emil <jo.emil@skriveleif.com>
2014-03-24 07:12:25 +0100

    Revert "Something in safespot isn't working right"
    This reverts commit a188acb237a0ffc2ef66b0a560beb92687be6c52.

--- server/gamestate.js
+++ server/gamestate.js
@@ -85,7 +85,6 @@ gamestate.prototype.PlanPath = function()
         {
             this.Write(n.move(0));
             this.Write("BOMB\n");
-            this.PlanBombs();
         } else {
             this.Write(n.move(0));
         }
@@ -100,13 +99,11 @@ gamestate.prototype.PlanBombs = function()
     // Can it hit me?
     if (this.SafeSpot(this.me.x, this.me.y) == false)
     {
-        var t = this.SquareSearch(1);
+        var t = this.SquareSearch(2);
         //console.log(t);
         this.target = [t.x, t.y];
-        console.log(this.target);
+        //console.log(this.target);
         this.flee = true;
-    } else {
-        console.log("You're safe...");
     }
     this.flee = false;
 }
@@ -114,38 +111,42 @@ gamestate.prototype.PlanBombs = function()
 gamestate.prototype.SquareSearch = function(r)
 {
     var distArr = [];
-    var m = this.map;
-    while (distArr.length == 0)
+
+    var start = {x: this.me.x - r, y: this.me.y - r}
+    var stop = {x: this.me.x + r, y: this.me.y + r}
+    if (start.x < 0) {start.x = 0;}
+    if (start.y < 0) {start.y = 0;}
+    if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
+    if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
+
+    for (var x = start.x; x <= stop.x; x++)
     {
-        var start = {x: this.me.x - r, y: this.me.y - r}
-        var stop = {x: this.me.x + r, y: this.me.y + r}
-        if (start.x < 0) {start.x = 0;}
-        if (start.y < 0) {start.y = 0;}
-        if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
-        if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
-
-        for (var x = start.x; x <= stop.x; x++)
+        for (var y = start.y ; y <= stop.y; y++)
         {
-            for (var y = start.y ; y <= stop.y; y++)
-            {
-                var d = lineDistance({x: this.me.x, y: this.me.y}, {x: x, y: y})
+            //console.log(x +", "+y);
+            var d = lineDistance({x: this.me.x, y: this.me.y}, {x: x, y: y})
 
-                if (this.SafeSpot(x, y) == true)
-                {
-                    m[y][x] = "S";
+            if (this.SafeSpot(x, y) == true)
+            {
+                try{
                     if (this.map[y][x] == 1)
                     {
-/*                        if (x == this.me.x && y == this.me.y)
+                        if (x == this.me.x && y == this.me.y)
                         {
                             return {x: x, y: y, d: d};
-                        }*/
+                        }
                         distArr.push({x: x, y: y, d: d});
                     }
-                } else { m[y][x] = "U"; }
+                } catch (err)
+                {
+                    console.log(err);
+                    console.log("x: "+x+", y: "+y);
+                    console.log(this.map);
+                }
             }
         }
-        r++;
     }
+
     distArr.sort(function(a, b) {return a[2] - b[2]});
     return distArr[0];
 }
@@ -181,6 +182,30 @@ gamestate.prototype.SafeSpot = function(x,y)
     }
 
     return safe;
+/*    for (var i = 0; i < this.bombs.length; i++)
+    {
+        var b = this.bombs[i];
+        if (b.y - 2 <= y && b.y + 2 >= y)
+        {
+            if (b.x - 2 <= x && b.x + 2 >= x)
+            {
+*//*                var m = this.map;
+                m[b.y-2][b.x] = 'O';
+                m[b.y-1][b.x] = 'O';
+                m[b.y][b.x] = 'O';
+                m[b.y+1][b.x] = 'O';
+                m[b.y+2][b.x] = 'O';
+                m[b.y][b.x-1] = 'O';
+                m[b.y][b.x-2] = 'O';
+                m[b.y][b.x+1] = 'O';
+                m[b.y][b.x+2] = 'O';
+                m[y][x] = 'X';
+                console.log(m);*//*
+                return false;
+            }
+        }
+    }
+    return true;*/
 }
 
 function lineDistance( point1, point2 )
