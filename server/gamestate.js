cfe439624f31bf03ef880c9f3ab84e34a2e4fc41
Jo Emil <jo.emil@skriveleif.com>
2014-03-27 13:19:57 +0100

    "Fixes" and "implementations"

--- server/gamestate.js
+++ server/gamestate.js
@@ -14,7 +14,8 @@ function gamestate(socket)
     this.me = {x: 0, x: 0};
     this.target = [];
     this.fear = false;
-    this.result = 0
+    this.result = 0;
+    this.armageddon = false;
     this.insults = ["Piggy!\n", "Waffles?\n", "AND I'M... AAAH-ahah... I dunno.\n", "I love this show!\n", "Tell me a story about giant pigs!\n", "I'm gonna sing the Doom Song now.\n", "Awww... I wanted to explode.\n",
         "Somebody needs a hug!\n", "..MONKEY!\n", "I made mashed po-ta-toes!\n", "Cazzo duro!\n",  "I miss my cupcake.\n", "Your head smells like a puppy!\n", "The pig... COMMANDS ME!\n", "Hi floor! Make me a sandwich!\n", "Aww, it likes me.\n", "TACOOOS!!!\n"];
 
@@ -23,78 +24,6 @@ function gamestate(socket)
 /**
  * Receive data from server, and start doing stuff
  */
-
-gamestate.prototype.SquareSearch = function(origo, r)
-{
-    var distArr = [];
-    console.log("In SquareSearch new")
-
-    // Prevent search from going outside map
-    var start = {x: origo.x - r, y: origo.y - r};
-    var stop = {x: origo.x + r, y: origo.y + r};
-    if (start.x < 0) {start.x = 0;}
-    if (start.y < 0) {start.y = 0;}
-    if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
-    if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
-
-    for (var x = start.x; x <= stop.x; x++)
-    {
-        for (var y = start.y ; y <= stop.y; y++)
-        {
-
-            // Is this a safe spot?
-            if (this.SafeSpot(x, y) == true)
-            {
-                if (this.map[y][x] == 1)
-                {
-
-                    distArr.push ({x:x, y:y})
-                }
-            }
-        }
-    }
-   
-    return distArr;
-}
-
-gamestate.prototype.playerMoveSearch = function(origo)
-{
-    var distArr = [];
-    console.log("In PlayerMoveSearch")
-
-    // Prevent search from going outside map
-    var start = {x: origo.x - 1, y: origo.y - 1};
-    var stop = {x: origo.x + 1, y: origo.y + 1};
-    if (start.x < 0) {start.x = 0;}
-    if (start.y < 0) {start.y = 0;}
-    if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
-    if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
-
-    var list = []
-    list.x = [0,-1,0,1,0]
-    list.y = [-1,0,0,0,1]
-
-    
-    for (var i = 0; i < 4;i++)
-            // Is this a safe spot?
-            x = origo.x + list.x[i];
-            y = origo.y + list.y[i];
-
-            if (this.SafeSpot(x, y) == true)
-            {
-                if (this.map[y][x] == 1)
-                {
-
-                    distArr.push ({x:x, y:y})
-                }
-    }
-
-   
-
-    return distArr;
-}
-
-
 gamestate.prototype.Update = function(data)
 {
     if (data.type == "status update") {
@@ -128,28 +57,62 @@ gamestate.prototype.Update = function(data)
 
             if (this.bombs.length > this.players.length + 1)
             {
+                this.armageddon = true;
                 console.log("ARMAGEDDON!!!");
                 this.target = [this.me.x, this.me.y];
+                /** Fixed **/
                 //vi burde gjøre armageddon-mode til true frem til end round slik at den ikke hopper mellom
                 //begge states hele tiden -Stian 
 
                 // Hvis nærmere enn 3 ruter til en person, gå i motsatt retning
+                if (this.SafeFromPlayers(this.me.x, this.me.y, 3) == true)
+                {
+                    this.WeightPlayers(2);
+                    // Start squaresearch så langt unna som mulig (men ikke i et hjørne -S), dekrementer hvis ingen treff
+                    // Prøv helst å finne treff i retningen fra folk
 
-                // Start squaresearch så langt unna som mulig (men ikke i et hjørne -S), dekrementer hvis ingen treff
-                // Prøv helst å finne treff i retningen fra folk
-                // Hvor hardt kan vi eventuelt vekte folk?
-                // Max 75% av kartet kan være 0 f.eks?
+                    var t = this.SquareSearch(this.me, 3);
 
-                // Her må også en algo for å se om man har blitt presset inn i et hjørne
-                // kjøres. Hvis ja må det lettes på trykket så man kommer seg unna
+                    if (t.length > 0)
+                    {
+                        var g = new Graph(this.map);
+                        var start = g.nodes[this.me.y][this.me.x];
 
-                // Sjekk om man kan gå til treff lengst unna
+                        var results = [];
 
-                // Gå dit
+                        //do an a* on all safespots and determine the closest one
+                        for (var i = 0; i < t.length; i++)
+                        {
+                            var end = tgraph.nodes[t[i].y][t[i].x];
+                            var result = astar.search(g.nodes, start, end);
+
+                            if (result.length > 0)
+                            {
+                                results.push({l: result.length, i: i});
+                            }
+                        }
+
+                        results.sort(function(a,b){ if (a.l < b.l) return -1; if (a.l > b.l) return 1; return 0; })
+
+                    }
+
+                    /** Prøver med å bare vekte selve spilleren som 0 **/
+                    // Hvor hardt kan vi eventuelt vekte folk?
+                    // Max 75% av kartet kan være 0 f.eks?
+
+                    /** Vekter bare selve spilleren som 0, resten blir vektet høyt - vi blir dermed aldri stuck **/
+                    // Her må også en algo for å se om man har blitt presset inn i et hjørne
+                    // kjøres. Hvis ja må det lettes på trykket så man kommer seg unna
+
+                } else {
+                    this.target = [this.me.x, this.me.y];
+                }
+
+                /** Fixed **/
                 //eventuelt sette target til seg selv om det er safe, og bare la passive do(d)ge mode gjøre jobben sin best mulig -Stian
 
-            } //else {
-                if (this.SafeSpot(this.me.x, this.me.y) == false)
+            } else {
+                if (this.SafeFromBombs(this.me.x, this.me.y) == false && this.armageddon == false)
                 {
                     console.log("UNSAFE");
                     this.fear = true;
@@ -181,10 +144,9 @@ gamestate.prototype.Update = function(data)
                             {
                                 arrays.push({l: result.length, i: i});
                             }
-
                         }
 
-                        //find lowest length
+/*                        //find lowest length
                         var p = 1000;
 
                         for (var i = 0; i<arrays.length; i++)
@@ -195,7 +157,7 @@ gamestate.prototype.Update = function(data)
                                 p = arrays[i].l;
                             }
 
-                        }
+                        }*/
 
                         //sort the array, lowest l first
                         arrays.sort(function(a,b){ if (a.l < b.l) return -1; if (a.l > b.l) return 1; return 0; })
@@ -222,7 +184,7 @@ gamestate.prototype.Update = function(data)
                         try {
                             this.result = samelength[0].i;
                         } catch (err) {
-                            this.write("TACOOOS!!!111!11one\n");
+                            this.Write("TACOOOS!!!111!11one\n");
                         }
 
                         //use the index of the smallest list to determine the move
@@ -243,7 +205,7 @@ gamestate.prototype.Update = function(data)
                     console.log(data.bombs);
                     this.fear = false;
                 }
-            //}
+            }
         }
 
         // Define a new a* graph
@@ -262,25 +224,25 @@ gamestate.prototype.Update = function(data)
         {
             var dontmove = false;
 
-            if (this.SafeSpot(this.me.x, this.me.y) == true) {
+            if (this.SafeFromBombs(this.me.x, this.me.y) == true) {
                
                 if (n.move(0) == "UP\n") {
-                    if (this.SafeSpot(this.me.x, this.me.y - 1) == false) {
+                    if (this.SafeFromBombs(this.me.x, this.me.y - 1) == false) {
                         dontmove = true;
                     }
                 }
                 if (n.move(0) == "DOWN\n") {
-                    if (this.SafeSpot(this.me.x, this.me.y + 1) == false) {
+                    if (this.SafeFromBombs(this.me.x, this.me.y + 1) == false) {
                         dontmove = true;
                     }
                 }
                 if (n.move(0) == "RIGHT\n") {
-                    if (this.SafeSpot(this.me.x + 1, this.me.y) == false) {
+                    if (this.SafeFromBombs(this.me.x + 1, this.me.y) == false) {
                         dontmove = true;
                     }
                 }
                 if (n.move(0) == "LEFT\n") {
-                    if (this.SafeSpot(this.me.x-1, this.me.y) == false) {
+                    if (this.SafeFromBombs(this.me.x-1, this.me.y) == false) {
                         dontmove = true;
                     }
                 }
@@ -289,7 +251,7 @@ gamestate.prototype.Update = function(data)
             if (dontmove == false) {
                 this.Write(n.move(0));}
             else {
-                this.write(this.insults[Math.floor(Math.random() * this.insults.length)]);
+                this.Write(this.insults[Math.floor(Math.random() * this.insults.length)]);
             }
             if (n.NextTile(0) == "ROCK")
             {
@@ -298,7 +260,7 @@ gamestate.prototype.Update = function(data)
         }
 
     } else if (data.type == "end round") {
-
+        this.armageddon = false;
     } else if (data.type == "dead") {
         console.log ("this bot is apparently dead");
         var deadlist = ["but.. whyy?\n", "MORRADI ER FEIT!!\n", "Next time, mr bond!\n", "dafuq?\n", "Your mom!\n", "My plan has failed!\n"];
@@ -308,13 +270,82 @@ gamestate.prototype.Update = function(data)
     }
 }
 
+gamestate.prototype.SquareSearch = function(origo, r)
+{
+    var distArr = [];
+    console.log("In SquareSearch new")
+
+    // Prevent search from going outside map
+    var start = {x: origo.x - r, y: origo.y - r};
+    var stop = {x: origo.x + r, y: origo.y + r};
+    if (start.x < 0) {start.x = 0;}
+    if (start.y < 0) {start.y = 0;}
+    if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
+    if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
+
+    for (var x = start.x; x <= stop.x; x++)
+    {
+        for (var y = start.y ; y <= stop.y; y++)
+        {
+
+            // Is this a safe spot?
+            if (this.SafeFromBombs(x, y) == true)
+            {
+                if (this.map[y][x] == 1)
+                {
+
+                    distArr.push ({x:x, y:y})
+                }
+            }
+        }
+    }
+
+    return distArr;
+}
+
+gamestate.prototype.playerMoveSearch = function(origo)
+{
+    var distArr = [];
+    console.log("In PlayerMoveSearch")
+
+    // Prevent search from going outside map
+    var start = {x: origo.x - 1, y: origo.y - 1};
+    var stop = {x: origo.x + 1, y: origo.y + 1};
+    if (start.x < 0) {start.x = 0;}
+    if (start.y < 0) {start.y = 0;}
+    if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
+    if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
+
+    var list = []
+    list.x = [0,-1,0,1,0]
+    list.y = [-1,0,0,0,1]
+
+
+    for (var i = 0; i < 4;i++)
+        // Is this a safe spot?
+        x = origo.x + list.x[i];
+    y = origo.y + list.y[i];
+
+    if (this.SafeFromBombs(x, y) == true)
+    {
+        if (this.map[y][x] == 1)
+        {
+
+            distArr.push ({x:x, y:y})
+        }
+    }
+
+
+
+    return distArr;
+}
+
 gamestate.prototype.WeightBombs = function()
 {
     for (var i = 0; i < this.bombs.length; i++)
     {
         var b = this.bombs[i];
         this.map[b.y][b.x] = 0;
-
 
         var start = {x: b.x - 2, y: b.y - 2};
         var stop = {x: b.x + 2, y: b.y + 2};
@@ -335,13 +366,49 @@ gamestate.prototype.WeightBombs = function()
     }
 }
 
+gamestate.prototype.WeightPlayers = function(r)
+{
+    for (var i = 0; i < this.players.length; i++)
+    {
+        var p = this.players[i];
+        this.map[p.y][p.x] = 0;
+
+        var start = {x: p.x - r, y: p.y - r};
+        var stop = {x: p.x + r, y: p.y + r};
+        if (start.x < 0) {start.x = 0;}
+        if (start.y < 0) {start.y = 0;}
+        if (stop.x > this.map[0].length - 1) {stop.x = this.map[0].length - 1;}
+        if (stop.y > this.map.length - 1) {stop.y = this.map.length - 1;}
+
+        for (var x = start.x; x <= stop.x; x++)
+        {
+            for (var y = start.y ; y <= stop.y; y++)
+            {
+                if (x == p.x && y == p.y)
+                {
+                    this.map[y][x] = 0;
+                }
+                else if ((x == p.x + 1 || x == p.x - 1) && (y == p.y + 1 || y == p.y - 1))
+                {
+                    this.map[y][x] = 10;
+                }
+                else if ((x == p.x + 2 || x == p.x - 2) && (y == p.y + 2 || y == p.y - 2))
+                {
+                    this.map[y][x] = 5;
+                }
+            }
+        }
+
+    }
+}
+
 
 /**
  * Check if a coordinate can be hit by a bomb
  */
-gamestate.prototype.SafeSpot = function(x,y)
+gamestate.prototype.SafeFromBombs = function(x,y)
 {
-//    console.log("in SafeSpot")
+//    console.log("in SafeFromBombs")
 
     var safe = true;
     for (var i = 0; i < this.bombs.length; i++)
@@ -361,26 +428,22 @@ gamestate.prototype.SafeSpot = function(x,y)
     return safe;
 }
 
-gamestate.prototype.checkForEnemiesInRadius = function(rad)
+gamestate.prototype.SafeFromPlayers = function(x,y,r)
 {
-    var boom = false;
+    var safe = true;
     for (var i = 0; i < this.players.length; i++)
     {
         var p = this.players[i];
 
-        var b = this.me;
-
-        if (p.x == b.x && b.y -rad <= p.y && b.y +rad >= p.y)
+        if (x <= p.x - r && x >= p.x + r)
         {
-            boom = true;
-        }
-
-        if (p.y == b.y && b.x -rad <= p.x && b.x +rad >= p.x)
-        {
-            boom = true;
+            if (y <= p.y - r && y >= p.y + r)
+            {
+                safe = false;
+            }
         }
     }
-    return boom;
+    return safe;
 }
 
 gamestate.prototype.CanGetThere = function(x, y, target, bombs)
@@ -420,11 +483,6 @@ gamestate.prototype.Write = function(input)
     console.log(input);
 }
 
-gamestate.prototype.write = function(input)
-{
-    this.socket.write(input);
-    console.log(input);
-}
 /**
  * Returns the distance from one {x: x, y: y} to another.
  */
